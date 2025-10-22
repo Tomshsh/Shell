9f7b57887f215ed95765a7090613dcb018f8eb1f
Tomer Shmatman <tomer.s@jenovice.com>
2025-10-22 21:42:27 +0300

    REPL

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,13 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-
-  std::cout << input << ": command not found" << std::endl;
+  while (true)
+  {
+    std::cout << "$ ";
+  
+    std::string input;
+    std::getline(std::cin, input);
+  
+    std::cout << input << ": command not found" << std::endl;
+  }
 }
