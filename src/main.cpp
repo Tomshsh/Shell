5f30a3ef385afe49b965685f0a05a8e8cbaa7405
Tomer Shmatman <tomer.s@jenovice.com>
2025-10-25 13:38:45 +0300

    locate executable

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <unordered_map>
 #include <sstream>
 #include <functional>
+#include <vector>
+#include <filesystem>
+#include <unistd.h>
+
+namespace fs = std::filesystem;
+
+/////////////////////////////# DECLARATIONS #///////////////////////////////
 
 enum command_type
 {
@@ -13,12 +20,24 @@ struct Command
 {
 	std::string command;
 	command_type type;
-	std::function<void(std::stringstream&)> cb;
+	std::function<void(std::stringstream &)> cb;
 
-	Command(std::string cmd, command_type cmd_type, std::function<void(std::stringstream&)> callback) : command(cmd), type(cmd_type), cb(std::move(callback)) {};
+	Command(std::string cmd, command_type cmd_type, std::function<void(std::stringstream &)> callback) : command(cmd), type(cmd_type), cb(std::move(callback)) {};
 };
 
 std::unordered_map<std::string, Command> commandMap;
+
+const char *path_delimiter = []{
+	#ifdef _WIN32
+		return ";";
+	#else
+		return ":";
+	#endif
+}();
+
+char *p_env = {};
+
+/////////////////////////////# UTILS #///////////////////////////////
 
 std::string TypeToStr(command_type cmd)
 {
@@ -32,7 +51,24 @@ std::string TypeToStr(command_type cmd)
 	}
 }
 
-void handleExit(std::stringstream& ss)
+std::vector<std::string> split(std::string str, std::string delim)
+{
+	std::vector<std::string> arr;
+	char *d_str = str.data(), *d_delim = delim.data();
+	char *tok = strtok(d_str, d_delim);
+
+	while (tok != NULL)
+	{
+		arr.push_back(std::string(tok));
+		tok = strtok(NULL, d_delim);
+	}
+
+	return arr;
+}
+
+/////////////////////////////# METHODS #///////////////////////////////
+
+void handleExit(std::stringstream &ss)
 {
 	std::string code;
 	ss >> code;
@@ -49,30 +85,58 @@ void handleExit(std::stringstream& ss)
 	exit(exit_code);
 }
 
-void handleEcho(std::stringstream& ss)
+void handleEcho(std::stringstream &ss)
 {
 	ss.seekg(5);
 	std::cout << ss.rdbuf() << "\n";
 }
 
-void handleType(std::stringstream& ss)
+void handleType(std::stringstream &ss)
 {
 	std::string com;
 	ss >> com;
+
+	std::string env_p = p_env ? p_env : "";
+
 	auto it = commandMap.find(com);
-	if (it == commandMap.end()){
+	if (it != commandMap.end())
+	{
+		printf("%s is a shell %s\n", com.c_str(), TypeToStr(it->second.type).c_str());
+	}
+	else
+	{
+		std::vector<std::string> arr = split(env_p, path_delimiter);
+		for (std::string str : arr)
+		{
+			const fs::path pth(str);
+			if (!fs::exists(pth))
+				continue;
+
+			for (auto const &dir_entry : fs::recursive_directory_iterator(pth, fs::directory_options::skip_permission_denied))
+			{
+				if (com == dir_entry.path().filename().string())
+				{
+					if (access(dir_entry.path().c_str(), X_OK) == 0)	// is executable
+					{
+						std::cout << com << " is " << dir_entry.path().string() << "\n";
+						return;
+					}
+				}
+			}
+		}
+	}
 		printf("%s: not found\n", com.c_str());
 		return;
 	}
-	printf("%s is a shell %s\n", com.c_str(), TypeToStr(it->second.type).c_str());
-}
 
-void initCommandMap() {
+/////////////////////////////# MAIN #///////////////////////////////
+
+void initCommandMap()
+{
 	commandMap = {
 		{"exit", Command("exit", BUILTIN, handleExit)},
 		{"echo", Command("echo", BUILTIN, handleEcho)},
-		{"type", Command("type", BUILTIN, handleType)}
-	};
+		{"type", Command("type", BUILTIN, handleType)}};
 }
 
 int main()
@@ -82,6 +146,10 @@ int main()
 	std::cerr << std::unitbuf;
 
 	initCommandMap();
+
+
+	p_env = std::getenv("PATH");
+	std::string env_p = p_env ? p_env : "";
 
 	while (true)
 	{
