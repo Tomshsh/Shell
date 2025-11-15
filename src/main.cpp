b378c3e271c7f94cef4ab21e4f8a74a9166261a6
Tomer Shmatman <tomer.s@jenovice.com>
2025-11-15 10:09:55 +0200

    executable completion

--- src/main.cpp
+++ src/main.cpp
@@ -13,29 +13,11 @@
 
 namespace fs = std::filesystem;
 
-/////////////////////////////# DECLARATIONS #///////////////////////////////
-
-const char *path_delimiter = []
-{
-#ifdef _WIN32
-	return ";";
-#else
-	return ":";
-#endif
-}();
-
-char *p_env = {};
-
-/////////////////////////////# MAIN #///////////////////////////////
-
 int main()
 {
 	// Flush after every cout / std:cerr
 	std::cout << std::unitbuf;
 	std::cerr << std::unitbuf;
-
-	p_env = getenv("PATH");
-	std::string env_p = p_env ? p_env : "";
 
 	while (true)
 	{
